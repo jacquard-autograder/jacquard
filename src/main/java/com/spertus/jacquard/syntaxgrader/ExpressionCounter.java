4ff2ad8a4676917c341f7c9e1238850a2b798722
Ellen Spertus <ellen.spertus@gmail.com>
2023-07-21 11:34:46 -0700

    Make name argument optional

--- src/main/java/com/spertus/jacquard/syntaxgrader/ExpressionCounter.java
+++ src/main/java/com/spertus/jacquard/syntaxgrader/ExpressionCounter.java
@@ -13,8 +13,8 @@ import java.util.List;
 public class ExpressionCounter extends ExpressionStatementCounter {
 
     /**
-     * Creates a new counter to test whether the number of occurrences of an
-     * expression type is within the specified range.
+     * Creates a new counter with the given name to test whether the number of
+     * occurrences of an expression type is within the specified range.
      *
      * @param name     the name of this processor (for the {@link Result})
      * @param maxScore the score if the condition holds
@@ -23,10 +23,27 @@ public class ExpressionCounter extends ExpressionStatementCounter {
      *                 if there is no limit
      * @param clazz    the expression class
      * @throws ClientException if minCount &lt; 0, maxCount &lt; minCount,
-     *                                              or minCount is 0 when maxCount is {@link Integer#MAX_VALUE}
+     *                         or minCount is 0 when maxCount is {@link Integer#MAX_VALUE}
      */
     public ExpressionCounter(String name, double maxScore, int minCount, int maxCount, Class<? extends Expression> clazz)
             throws ClientException {
         super(name, clazz.getSimpleName(), maxScore, minCount, maxCount, List.of(clazz), List.of());
+    }
+
+    /**
+     * Creates a new counter with a default name to test whether the number of
+     * occurrences of an expression type is within the specified range.
+     *
+     * @param maxScore the score if the condition holds
+     * @param minCount the minimum number of occurrences
+     * @param maxCount the maximum number of occurrences, or {@link Integer#MAX_VALUE}
+     *                 if there is no limit
+     * @param clazz    the expression class
+     * @throws ClientException if minCount &lt; 0, maxCount &lt; minCount,
+     *                         or minCount is 0 when maxCount is {@link Integer#MAX_VALUE}
+     */
+    public ExpressionCounter(double maxScore, int minCount, int maxCount, Class<? extends Expression> clazz)
+            throws ClientException {
+        this(clazz.getSimpleName() + "counter", maxScore, minCount, maxCount, clazz);
     }
 }
--- src/main/java/com/spertus/jacquard/syntaxgrader/StatementCounter.java
+++ src/main/java/com/spertus/jacquard/syntaxgrader/StatementCounter.java
@@ -13,8 +13,8 @@ import java.util.List;
 public class StatementCounter extends ExpressionStatementCounter {
 
     /**
-     * Creates a new counter to test whether the number of occurrences of the
-     * statement type is within the specified range.
+     * Creates a new counter with the given name to test whether the number of
+     * occurrences of the statement type is within the specified range.
      *
      * @param name     the name of this processor (for the {@link Result})
      * @param maxScore the score if the condition holds
@@ -33,5 +33,26 @@ public class StatementCounter extends ExpressionStatementCounter {
             Class<? extends Statement> clazz
     ) throws ClientException {
         super(name, clazz.getSimpleName(), maxScore, minCount, maxCount, List.of(), List.of(clazz));
+    }
+
+    /**
+     * Creates a new counter with a default name to test whether the number of
+     * occurrences of the statement type is within the specified range.
+     *
+     * @param maxScore the score if the condition holds
+     * @param minCount the minimum number of occurrences, which must be non-negative
+     * @param maxCount the maximum number of occurrences, or {@link Integer#MAX_VALUE}
+     *                 if there is no limit
+     * @param clazz    the statement class
+     * @throws ClientException if minCount &lt; 0, maxCount &lt; minCount,
+     *                         or minCount is 0 when maxCount is {@link Integer#MAX_VALUE}
+     */
+    public StatementCounter(
+            double maxScore,
+            int minCount,
+            int maxCount,
+            Class<? extends Statement> clazz
+    ) throws ClientException {
+        this(clazz.getSimpleName() + " counter", maxScore, minCount, maxCount, clazz);
     }
 }
